5bf1fa51e7fc78073b63a95f03a36725ad07fe61
saifmahin500 <saifmahin500@gmail.com>
2023-10-18 01:44:03 +0600

    i import my all setup and creat some route

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../header/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is home page</h2>
+            <Banner></Banner>
+           
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/header/Banner.jsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <h4>this is banner section</h4>
+        </div>
+    );
+};
+
+export default Banner;
