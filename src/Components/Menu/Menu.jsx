046dff54f9cf982b29fb40815764b811383b05eb
saifmahin500 <saifmahin500@gmail.com>
2023-10-18 03:37:23 +0600

    i added my banner section

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+
+
+const Menu = () => {
+    return (
+        <div>
+            <h2 className="text-center text-3xl font-bold my-10">Our Special Menu</h2>
+        </div>
+    );
+};
+
+export default Menu;
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import Menu from "../Menu/Menu";
 import Banner from "../header/Banner";
 
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>       
             <Banner></Banner>
+            <Menu></Menu>
            
         </div>
     );
